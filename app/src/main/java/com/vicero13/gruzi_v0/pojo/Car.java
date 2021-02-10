62fa2cb984166be9d438951f07cc87d325bd9e9f
Robert <rbeygel@gmail.com>
2021-02-10 18:13:32 +0300

    First commit

---
+++ app/src/main/java/com/vicero13/gruzi_v0/pojo/Car.java
@@ -0,0 +1,67 @@
+package com.vicero13.gruzi_v0.pojo;
+
+public class Car {
+    private String model;
+    private int lengthCarcase;
+    private int widthCarcase;
+    private int heightCarcase;
+    private int capacityCarcase;
+    private int maxWeightCarcase;
+    private String numberplate;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getLengthCarcase() {
+        return lengthCarcase;
+    }
+
+    public void setLengthCarcase(int lengthCarcase) {
+        this.lengthCarcase = lengthCarcase;
+    }
+
+    public int getWidthCarcase() {
+        return widthCarcase;
+    }
+
+    public void setWidthCarcase(int widthCarcase) {
+        this.widthCarcase = widthCarcase;
+    }
+
+    public int getHeightCarcase() {
+        return heightCarcase;
+    }
+
+    public void setHeightCarcase(int heightCarcase) {
+        this.heightCarcase = heightCarcase;
+    }
+
+    public int getCapacityCarcase() {
+        return capacityCarcase;
+    }
+
+    public void setCapacityCarcase(int capacityCarcase) {
+        this.capacityCarcase = capacityCarcase;
+    }
+
+    public int getMaxWeightCarcase() {
+        return maxWeightCarcase;
+    }
+
+    public void setMaxWeightCarcase(int maxWeightCarcase) {
+        this.maxWeightCarcase = maxWeightCarcase;
+    }
+
+    public String getNumberplate() {
+        return numberplate;
+    }
+
+    public void setNumberplate(String numberplate) {
+        this.numberplate = numberplate;
+    }
+}
---
+++ app/src/main/java/com/vicero13/gruzi_v0/pojo/User.java
@@ -0,0 +1,65 @@
+package com.vicero13.gruzi_v0.pojo;
+
+public class User {
+    private int id;
+    private String name;
+    private int phoneNumber;
+    private String password;
+    private String email;
+    private Car car;
+
+    public User(int id, String name, int phoneNumber, String password) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+}
